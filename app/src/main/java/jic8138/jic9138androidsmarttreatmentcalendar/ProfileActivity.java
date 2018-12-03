f1ae485e7a65013f52d12c808ec859f53cf254a7
sdeval3 <sdeval3@gatech.edu>
2018-12-02 22:06:00 -0500

    Added Back Button to Profile Page

--- app/src/main/java/jic8138/jic9138androidsmarttreatmentcalendar/ProfileActivity.java
+++ app/src/main/java/jic8138/jic9138androidsmarttreatmentcalendar/ProfileActivity.java
@@ -1,9 +1,12 @@
 package jic8138.jic9138androidsmarttreatmentcalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +22,16 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_screen);
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ProfileIntent = new Intent(ProfileActivity.this, CalendarActivity.class);
+                finish();
+                startActivity(ProfileIntent);
+            }
+        });
 
 //        TextView email = findViewById(R.id.email_actual);
 //        email.setText(currentUser.getEmail());
