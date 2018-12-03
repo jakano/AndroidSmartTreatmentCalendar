9e1ca35dfaaa9f99f69ecea6ac3e572feae89e8c
jakano <jakano3@gatech.edu>
2018-12-02 21:31:16 -0500

    Fixed bug where navigation to profile page will log the user out.

--- app/src/main/java/jic8138/jic9138androidsmarttreatmentcalendar/CalendarActivity.java
+++ app/src/main/java/jic8138/jic9138androidsmarttreatmentcalendar/CalendarActivity.java
@@ -12,30 +12,14 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.alamkanak.weekview.WeekViewEvent;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
-
-import jic8138.jic9138androidsmarttreatmentcalendar.Controllers.Database;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -45,16 +29,16 @@ public class CalendarActivity extends AppCompatActivity {
     private ArrayList<Event> e = new ArrayList<>();
     private Fragment mCalendarFragment;
     private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle toggler;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_layout);
         mDrawerLayout = findViewById(R.id.dl);
-        toggler = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(toggler);
-        toggler.syncState();
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mAddEventButton = findViewById(R.id.calendar_floating_add_button);
         mAddEventButton.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +47,8 @@ public class CalendarActivity extends AppCompatActivity {
                 onAddEventTap();
             }
         });
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
+        NavigationView drawerNavigationView = findViewById(R.id.nav_view);
+        drawerNavigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
@@ -74,13 +58,14 @@ public class CalendarActivity extends AppCompatActivity {
                                 Intent ProfileIntent = new Intent(CalendarActivity.this, ProfileActivity.class);
                                 finish();
                                 startActivity(ProfileIntent);
+                                break;
                             case R.id.navigation_logout:
                                 Intent HomescreenIntent = new Intent(CalendarActivity.this, HomeScreenActivity.class);
                                 HomescreenIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                 finish();
                                 startActivity(HomescreenIntent);
                                 FirebaseAuth.getInstance().signOut();
-
+                                break;
                         }
                         return true;
                     }
